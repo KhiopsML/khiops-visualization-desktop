f92a2a7e9a573ee6cbb2a725b96c998593b7ca74
BOUGET Stephane INNOV/IT-S <stephane.bouget@orange.com>
2026-02-19 15:04:16 +0100

    feat: update webpack configuration for Electron support and add Cypress support files

--- angular.webpack.js
+++ angular.webpack.js
@@ -1,35 +1,44 @@
 //Polyfill Node.js core modules in Webpack. This module is only needed for webpack 5+.
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 /**
  * Custom angular webpack configuration
  */
 module.exports = (config, options) => {
-    config.target = 'electron-renderer';
+  // Default to 'web' target for browser environments (e.g., e2e tests)
+  // Will be overridden to 'electron-renderer' if not using web environment
+  config.target = 'web';
 
-    if (options.fileReplacements) {
-        for(let fileReplacement of options.fileReplacements) {
-            if (fileReplacement.replace !== 'src/environments/environment.ts') {
-                continue;
-            }
+  if (options.fileReplacements) {
+    for (let fileReplacement of options.fileReplacements) {
+      if (fileReplacement.replace !== 'src/environments/environment.ts') {
+        continue;
+      }
 
-            let fileReplacementParts = fileReplacement['with'].split('.');
-            if (fileReplacementParts.length > 1 && ['web'].indexOf(fileReplacementParts[1]) >= 0) {
-                config.target = 'web';
-            }
-            break;
-        }
+      let fileReplacementParts = fileReplacement['with'].split('.');
+      // Use electron-renderer target only when NOT using web environment
+      if (
+        fileReplacementParts.length > 1 &&
+        ['web'].indexOf(fileReplacementParts[1]) >= 0
+      ) {
+        config.target = 'web';
+      } else {
+        // Only use electron-renderer for actual Electron builds (dev, prod environments)
+        config.target = 'electron-renderer';
+      }
+      break;
     }
+  }
 
-    config.plugins = [
-        ...config.plugins,
-        new NodePolyfillPlugin({
-			  excludeAliases: ["console"]
-		})
-    ];
+  config.plugins = [
+    ...config.plugins,
+    new NodePolyfillPlugin({
+      excludeAliases: ['console'],
+    }),
+  ];
 
-    // https://github.com/ryanclark/karma-webpack/issues/497
-    config.output.globalObject = 'globalThis';
+  // https://github.com/ryanclark/karma-webpack/issues/497
+  config.output.globalObject = 'globalThis';
 
-    return config;
-}
+  return config;
+};
