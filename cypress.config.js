51281aea89bf377d95fa6152c9c13c419473718b
Stéphane Bouget <stephane.bouget@orange.com>
2026-04-03 11:57:25 +0200

    chore: remove webpack preprocessor setup from Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,9 @@
 const { defineConfig } = require('cypress');
-const webpackPreprocessor = require('@cypress/webpack-preprocessor');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
     supportFile: false,
-    setupNodeEvents(on) {
-      on(
-        'file:preprocessor',
-        webpackPreprocessor({
-          webpackOptions: {
-            module: {
-              rules: [
-                {
-                  test: /\.ts$/,
-                  loader: 'ts-loader',
-                  options: { configFile: 'cypress/tsconfig.json' },
-                },
-              ],
-            },
-            resolve: { extensions: ['.ts', '.js'] },
-          },
-        }),
-      );
-    },
   },
 });
