ab962d6346a7cf798b5fd907e18bdfa80e6d10fb
Stéphane Bouget <stephane.bouget@orange.com>
2026-04-13 16:45:19 +0200

    fix: sign git tags in tag.sh script

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -12,7 +12,7 @@ sed -i "s/\"version\": \".*\"/\"version\": \"$TAG\"/" ./app/package.json
 
 git add .
 git commit -am "$TAG"
-git tag v$TAG
+git tag -s v$TAG
 git push origin main --tags
 
 # exit 0
